27bc79cffb779ea8a992906bade2fb5dafa8698f
Anjali Ratnam <anjaliratnam@microsoft.com>
2025-08-08 13:50:40 -0700

    updates

--- adlfs/spec.py
+++ adlfs/spec.py
@@ -2166,7 +2166,7 @@ class AzureBlobFile(AbstractBufferedFile):
                     length=len(chunk),
                 )
 
-    async def _async_upload_chunk(self, final: bool = False, **kwargs):
+    async def _async_upload_chunk(self, final: bool = False, max_conncurrency=None, **kwargs):
         """
         Write one part of a multi-block file upload
 
