651b6aef4c458efdd9502d35db92def4110b3ed3
Anjali Ratnam <anjaliratnam@microsoft.com>
2025-08-13 12:07:26 -0700

    updates

--- adlfs/scripts/perf/write/create_local_files.py
+++ adlfs/scripts/perf/write/create_local_files.py
@@ -1,11 +1,27 @@
 import time
 from io import BytesIO
+import os
+import torch
+
+from azstoragetorch.io import BlobIO
+from transformers import RobertaModel, BertModel
+from adlfs import AzureBlobFileSystem
+import fsspec
+import torch.nn as nn
+
+
+class TinyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc = nn.Linear(30, 30)  # very small layer
 
 
 def main():
-    content = b"1" * 5 * 1024**3
-    with open("local-models/5GB.txt", "wb") as f:
-        f.write(content)
+    # content = b"1" * 5 * 1024**3
+    model = TinyModel()
+    state = model.state_dict()
+    with open("5KiB-model.pth", "wb") as f:
+        torch.save(state, f)
   
 if __name__ == "__main__":
     main()
